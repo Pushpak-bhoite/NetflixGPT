473e744c8d0d7f9f7079407c8154830f9817c3c4
Pushpak-bhoite <bhoitepushpak6@gmail.com>
2024-10-19 18:28:32 +0530

    hello

---
+++ NetflixGPT-Typescript-Tailwind/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Section from './components/Section'
+
+function App() {
+
+  return (
+    <>
+     <div>
+        <Section />
+     </div>
+    </>
+  )
+}
+
+export default App
